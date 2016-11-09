3c07c412daaa662b88ff8ecc1a271d7043ea209b
Dave Wasmer <davewasmer@gmail.com>
2016-11-09 16:25:09 -0700

    refactor: change intializer to only establish connection and sync schema now that Denali has hooks for defining models

---
+++ config/initializers/node-orm2-connect.js
@@ -0,0 +1,23 @@
+import { fromNode } from 'bluebird';
+import orm from 'orm';
+
+export default {
+  name: 'node-orm2-connect',
+  before: 'define-orm-models',
+  async initialize(application) {
+    let container = application.container;
+    let adapter = container.lookup('orm-adapter:node-orm2');
+    let config = application.config.database;
+
+    try {
+      adapter.db = await fromNode((cb) => orm.connect(config.url, cb));
+    } catch (error) {
+      application.logger.error('Error initializing the node-orm2 adapter or database connection:');
+      application.logger.error(error.stack);
+    }
+
+    if (config.syncSchema) {
+      await fromNode((cb) => adapter.db.sync(cb));
+    }
+  }
+};
